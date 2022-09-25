fd6aecad0381dcf71e166d54961fdf26f544a2e5
solovyovsdom <joelborofsky@mac.com>
2022-09-24 21:15:41 -0500

    first commit

---
+++ public/src/js/main.js
@@ -0,0 +1,20 @@
+import '../styles/style.css';
+import '../styles/navbar.css';
+
+/*********************************/
+/*********    NAVBAR    **********/
+/*********************************/
+
+const burger = document.querySelector('.burger i');
+const nav = document.querySelector('.nav');
+
+
+function toggleNav() {
+    burger.classList.toggle('fa-bars')
+    burger.classList.toggle('fa-times')
+    nav.classList.toggle('nav-active')
+}
+
+burger.addEventListener('click', () => {
+    toggleNav()
+})
